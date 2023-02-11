d20a287a48ea762d88f0c08b90908db420c4de60
Delany <delany.middleton@traderoot.com>
2023-02-11 19:44:40 +0200

    Error-prone

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,30 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "DevOps Project for AchiStar Technologies";
+
+  public String sayHello() {
+    foo(Suit.CLUBS);
+    return "Errorprone issue reproducer";
+  }
+
+  enum Suit {
+    HEARTS,
+    CLUBS,
+    SPADES,
+    DIAMONDS
+  }
+
+  private void foo(Suit suit) {
+    switch (suit) {
+      case HEARTS:
+        System.out.println("Red hearts");
+      case DIAMONDS:
+        System.out.println("Red diamonds");
+      case SPADES:
+        // Fall through
+      case CLUBS:
+        System.out.println("Black suit");
     }
+  }
+
 }
